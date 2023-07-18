2e0f41cde5779334610fe00598f1dcb8a47de519
Siddwap <58450503+Siddwap@users.noreply.github.com>
2023-07-18 17:07:19 +0530

    Delete routers directory

--- routers/auth.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const authController = require('../controller/auth')
-
-router.post('/login', authController.loginController);
-router.post('/signup', authController.signupController);
-
-module.exports = router;
